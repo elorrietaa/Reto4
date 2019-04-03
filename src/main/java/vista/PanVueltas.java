17f595ba0b4dd48e49ec0389f8df1a84e418a7c7
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-04-03 12:01:46 +0200

    Terminado PanVueltas

---
+++ src/main/java/vista/PanVueltas.java
@@ -0,0 +1,55 @@
+package vista;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+
+public class PanVueltas extends JPanel {
+	public JTextField textField, textField_1;
+	public JLabel lblTotalAPagar, lblTotalIntroducido, lblVueltas;
+	public JTextPane PanelVueltas;
+	public JButton btnFinalizar;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanVueltas() {
+		setBackground(Color.LIGHT_GRAY);
+		setBounds(0, 0, 1024, 720);
+		setLayout(null);
+		
+		lblTotalAPagar = new JLabel("Total a pagar");
+		lblTotalAPagar.setBounds(356, 239, 80, 14);
+		add(lblTotalAPagar);
+		
+		lblTotalIntroducido = new JLabel("Total introducido");
+		lblTotalIntroducido.setBounds(356, 281, 80, 14);
+		add(lblTotalIntroducido);
+		
+		textField = new JTextField();
+		textField.setBounds(483, 239, 86, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(483, 281, 86, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		lblVueltas = new JLabel("Vueltas");
+		lblVueltas.setBounds(356, 328, 46, 14);
+		add(lblVueltas);
+		
+		PanelVueltas = new JTextPane();
+		PanelVueltas.setBounds(440, 328, 129, 159);
+		add(PanelVueltas);
+		
+		btnFinalizar = new JButton("Finalizar");
+		btnFinalizar.setBounds(415, 519, 89, 23);
+		add(btnFinalizar);
+	}
+}

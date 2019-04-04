621c58baeb9ab984270cdeecb4c7cdd77ad3b200
elorrietaa <alba.rodriguezsa@elorrieta-errekamari.com>
2019-04-04 11:55:08 +0200

    reorganizacion constructores

--- src/main/java/controlador/PrincipalControlador.java
+++ src/main/java/controlador/PrincipalControlador.java
@@ -1,4 +1,8 @@
 package controlador;
+
+import modelo.PrincipalModelo;
+import vista.JframePrincipal;
+
 /**
  * Clase PrincipalControlador:
  * Contiene las instancias de todas las clases del paquete controlador. 
@@ -6,5 +10,17 @@ package controlador;
  *
  */
 public class PrincipalControlador {
-    ControladorPanBuscarHotel controladorPanBuscarHotel = new ControladorPanBuscarHotel();
+    public PrincipalModelo modelo;
+    public JframePrincipal vista;
+    
+    private ControladorPanBuscarHotel panelBuscarHotel;
+    
+    public PrincipalControlador(PrincipalModelo modelo, JframePrincipal vista) {
+	this.modelo = modelo;
+	this.vista = vista;
+    }
+    
+    public void inicializarVista() {
+	vista.setVisible(true);
+    }
 }
--- src/main/java/controlador/PrincipalReto4.java
+++ src/main/java/controlador/PrincipalReto4.java
@@ -1,7 +1,7 @@
 package controlador;
 
 import modelo.PrincipalModelo;
-import vista.PrincipalVista;
+import vista.JframePrincipal;
 /**
  * Clase PrincipalReto4 = clase principal a partir de la que arranca todo el programa.
  * @author alba
@@ -12,9 +12,9 @@ public class PrincipalReto4 {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	
-	PrincipalControlador controlador = new PrincipalControlador();
 	PrincipalModelo modelo = new PrincipalModelo();
-	PrincipalVista vista = new PrincipalVista();
+	JframePrincipal vista = new JframePrincipal();
+	PrincipalControlador controlador = new PrincipalControlador(modelo, vista);
     } 
 
 }

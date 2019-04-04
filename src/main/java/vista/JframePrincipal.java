39d4e77d2d87c91c173aa364a1ad36a352892835
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-04-04 12:20:50 +0200

    Acabado el JFramePrincipal  (falta comentar)

--- src/main/java/vista/JframePrincipal.java
+++ src/main/java/vista/JframePrincipal.java
@@ -7,19 +7,41 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.io.IOException;
 
-public class JframePrincipal {
+public class JframePrincipal extends JFrame{
 
-	private JFrame frame;
+	public JFrame frame;
+	public PanBuscarHotel buscarHotel;
+	public PanDetallesReserva detallesReserva;
+	public PanPago pago;
+	public PanVueltas vueltas;
 
 	public JframePrincipal() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 1024, 720);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
+		
+		inicializarPaneles();
+		anadirPaneles();
 	}
 
-	private void initialize() {
-		
+	private void inicializarPaneles() {
+		//try {
+		buscarHotel = new PanBuscarHotel();
+		//}
+		/*catch(IOException e) {
+			e.printStackTrace();
+		}*/
+		detallesReserva = new PanDetallesReserva();
+		pago = new PanPago();
+		vueltas = new PanVueltas();
+	}
+	private void anadirPaneles() {
+		getContentPane().add(buscarHotel);
+		getContentPane().add(detallesReserva);
+		getContentPane().add(pago);
+		getContentPane().add(vueltas);
 	}
 }

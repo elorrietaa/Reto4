5075f7b1f89e4d14d07b16bc0261c54d2c77f9c5
elorrietaa <alba.rodriguezsa@elorrieta-errekamari.com>
2019-05-11 12:20:43 +0200

    movidos elementos panel pago para ordenarlos en la vista. todo funcionando

--- src/main/java/vista/PanPago.java
+++ src/main/java/vista/PanPago.java
@@ -20,6 +20,8 @@ public class PanPago extends JPanel {
 	public JLabel lblTotal, lblTotalIntroducido, lblAPagar, lbldeseaAplicarUn;
 	public JButton btnCancelar, btnAplicar, btnNoAplicar;
 	public JComboBox<Object> cBListaCupones;
+	private JTextField totalSindesc;
+	private JTextField totalCondesc;
 
 	/**
 	 * Create the panel.
@@ -30,91 +32,91 @@ public class PanPago extends JPanel {
 		setLayout(null);
 		
 		btn200 = new JButton("200 \u20AC");
-		btn200.setBounds(163, 188, 89, 23);
+		btn200.setBounds(251, 354, 89, 23);
 		add(btn200);
 		
 		btn100 = new JButton("100 \u20AC");
-		btn100.setBounds(163, 236, 89, 23);
+		btn100.setBounds(251, 402, 89, 23);
 		add(btn100);
 		
 		btn50 = new JButton("50 \u20AC");
-		btn50.setBounds(163, 284, 89, 23);
+		btn50.setBounds(251, 450, 89, 23);
 		add(btn50);
 		
 		btn20 = new JButton("20 \u20AC");
-		btn20.setBounds(163, 337, 89, 23);
+		btn20.setBounds(251, 503, 89, 23);
 		add(btn20);
 		
 		btn10 = new JButton("10 \u20AC");
-		btn10.setBounds(163, 388, 89, 23);
+		btn10.setBounds(251, 554, 89, 23);
 		add(btn10);
 		
 		btn5 = new JButton("5 \u20AC");
-		btn5.setBounds(163, 441, 89, 23);
+		btn5.setBounds(427, 354, 89, 23);
 		add(btn5);
 		
 		btn2 = new JButton("2 \u20AC");
-		btn2.setBounds(339, 284, 89, 23);
+		btn2.setBounds(427, 402, 89, 23);
 		add(btn2);
 		
 		btn1 = new JButton("1 \u20AC");
-		btn1.setBounds(339, 337, 89, 23);
+		btn1.setBounds(427, 455, 89, 23);
 		add(btn1);
 		
 		btn50cen = new JButton("0,5 \u20AC");
-		btn50cen.setBounds(513, 188, 89, 23);
+		btn50cen.setBounds(427, 506, 89, 23);
 		add(btn50cen);
 		
 		btn20cen = new JButton("0,2 \u20AC");
-		btn20cen.setBounds(513, 236, 89, 23);
+		btn20cen.setBounds(601, 354, 89, 23);
 		add(btn20cen);
 		
 		btn10cen = new JButton("0,1 \u20AC");
-		btn10cen.setBounds(513, 284, 89, 23);
+		btn10cen.setBounds(601, 402, 89, 23);
 		add(btn10cen);
 		
 		btn5cen = new JButton("0,05 \u20AC");
-		btn5cen.setBounds(513, 337, 89, 23);
+		btn5cen.setBounds(601, 455, 89, 23);
 		add(btn5cen);
 		
 		btn2cen = new JButton("0,02 \u20AC");
-		btn2cen.setBounds(513, 388, 89, 23);
+		btn2cen.setBounds(601, 506, 89, 23);
 		add(btn2cen);
 		
 		btn1cen = new JButton("0,01 \u20AC");
-		btn1cen.setBounds(513, 441, 89, 23);
+		btn1cen.setBounds(601, 559, 89, 23);
 		add(btn1cen);
 		
 		lblTotal = new JLabel("Total:");
 		lblTotal.setFont(new Font("Dialog", Font.BOLD, 20));
-		lblTotal.setBounds(175, 53, 65, 26);
+		lblTotal.setBounds(263, 245, 65, 26);
 		add(lblTotal);
 		
 		total = new JTextField();
 		total.setEditable(false);
-		total.setBounds(163, 88, 86, 20);
+		total.setBounds(251, 280, 86, 20);
 		add(total);
 		total.setColumns(10);
 		
 		lblTotalIntroducido = new JLabel("Total introducido:");
 		lblTotalIntroducido.setFont(new Font("Dialog", Font.BOLD, 20));
-		lblTotalIntroducido.setBounds(307, 58, 172, 16);
+		lblTotalIntroducido.setBounds(395, 250, 172, 16);
 		add(lblTotalIntroducido);
 		
 		totalIntro = new JTextField();
 		totalIntro.setEditable(false);
-		totalIntro.setBounds(339, 88, 86, 20);
+		totalIntro.setBounds(427, 280, 86, 20);
 		add(totalIntro);
 		totalIntro.setColumns(10);
 		
 		lblAPagar = new JLabel("A pagar:");
 		lblAPagar.setFont(new Font("Dialog", Font.BOLD, 20));
-		lblAPagar.setBounds(513, 50, 111, 33);
+		lblAPagar.setBounds(601, 242, 111, 33);
 		add(lblAPagar);
 		
 		aPagar = new JTextField();
 		aPagar.setEditable(false);
-		aPagar.setBounds(513, 88, 86, 20);
+		aPagar.setBounds(601, 280, 86, 20);
 		add(aPagar);
 		aPagar.setColumns(10);
 		
@@ -132,20 +134,40 @@ public class PanPago extends JPanel {
 		add(btnCancelar);
 		
 		cBListaCupones = new JComboBox<Object>();
-		cBListaCupones.setBounds(652, 88, 179, 20);
+		cBListaCupones.setBounds(53, 50, 316, 20);
 		add(cBListaCupones);
 		
 		lbldeseaAplicarUn = new JLabel("\u00BFDesea aplicar un cup\u00F3n de descuento?");
-		lbldeseaAplicarUn.setBounds(652, 63, 221, 14);
+		lbldeseaAplicarUn.setBounds(53, 25, 221, 14);
 		add(lbldeseaAplicarUn);
 		
 		btnAplicar = new JButton("Aplicar");
-		btnAplicar.setBounds(851, 87, 89, 23);
+		btnAplicar.setBounds(403, 49, 89, 23);
 		add(btnAplicar);
 		
 		btnNoAplicar = new JButton("No aplicar");
-		btnNoAplicar.setBounds(851, 127, 89, 23);
+		btnNoAplicar.setBounds(403, 92, 89, 23);
 		add(btnNoAplicar);
+		
+		totalSindesc = new JTextField();
+		totalSindesc.setEditable(false);
+		totalSindesc.setColumns(10);
+		totalSindesc.setBounds(601, 85, 86, 20);
+		add(totalSindesc);
+		
+		JLabel lblPrecioSinAplicar = new JLabel("Precio sin aplicar descuento:");
+		lblPrecioSinAplicar.setBounds(601, 49, 146, 14);
+		add(lblPrecioSinAplicar);
+		
+		totalCondesc = new JTextField();
+		totalCondesc.setEditable(false);
+		totalCondesc.setColumns(10);
+		totalCondesc.setBounds(774, 86, 86, 20);
+		add(totalCondesc);
+		
+		JLabel lblPrecioAplicandoDescuento = new JLabel("Precio aplicando descuento:");
+		lblPrecioAplicandoDescuento.setBounds(774, 50, 146, 14);
+		add(lblPrecioAplicandoDescuento);
 		btnContinuar.setVisible(false);
 	}
 }

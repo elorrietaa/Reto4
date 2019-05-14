08d73566a4596b24556a197e9bace3bfab58d7d1
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-05-14 09:09:02 +0200

    Creado y rellenado el objeto ServiciosAdicionales

---
+++ src/main/java/modelo/ServiciosAdicionales.java
@@ -0,0 +1,169 @@
+package modelo;
+
+public class ServiciosAdicionales {
+	
+	private boolean wifi;
+	private boolean aire;
+	private boolean piscina;
+	private boolean spa;
+	private boolean gimnasio;
+	private boolean parking;
+	
+	private float precioWifi;
+	private float precioAire;
+	private float precioPiscina;
+	private float precioSpa;
+	private float precioGimnasio;
+	private float precioParking;
+	
+	private boolean wifiSeleccionado;
+	private boolean aireSeleccionado;
+	private boolean piscinaSeleccionada;
+	private boolean spaSeleccionado;
+	private boolean gimnasioSeleccionado;
+	private boolean parkingSeleccionado;
+	
+	public boolean isWifi() {
+		return wifi;
+	}
+	
+	public void setWifi(boolean wifi) {
+		this.wifi = wifi;
+	}
+	
+	public boolean isAire() {
+		return aire;
+	}
+	
+	public void setAire(boolean aire) {
+		this.aire = aire;
+	}
+	
+	public boolean isPiscina() {
+		return piscina;
+	}
+	
+	public void setPiscina(boolean piscina) {
+		this.piscina = piscina;
+	}
+	
+	public boolean isSpa() {
+		return spa;
+	}
+	
+	public void setSpa(boolean spa) {
+		this.spa = spa;
+	}
+	
+	public boolean isGimnasio() {
+		return gimnasio;
+	}
+	
+	public void setGimnasio(boolean gimnasio) {
+		this.gimnasio = gimnasio;
+	}
+	
+	public boolean isParking() {
+		return parking;
+	}
+	
+	public void setParking(boolean parking) {
+		this.parking = parking;
+	}
+	
+	public float getPrecioWifi() {
+		return precioWifi;
+	}
+	
+	public void setPrecioWifi(float precioWifi) {
+		this.precioWifi = precioWifi;
+	}
+	
+	public float getPrecioAire() {
+		return precioAire;
+	}
+	
+	public void setPrecioAire(float precioAire) {
+		this.precioAire = precioAire;
+	}
+	
+	public float getPrecioPiscina() {
+		return precioPiscina;
+	}
+	
+	public void setPrecioPiscina(float precioPiscina) {
+		this.precioPiscina = precioPiscina;
+	}
+	
+	public float getPrecioSpa() {
+		return precioSpa;
+	}
+	
+	public void setPrecioSpa(float precioSpa) {
+		this.precioSpa = precioSpa;
+	}
+	
+	public float getPrecioGimnasio() {
+		return precioGimnasio;
+	}
+	
+	public void setPrecioGimnasio(float precioGimnasio) {
+		this.precioGimnasio = precioGimnasio;
+	}
+	
+	public float getPrecioParking() {
+		return precioParking;
+	}
+	
+	public void setPrecioParking(float precioParking) {
+		this.precioParking = precioParking;
+	}
+	
+	public boolean isWifiSeleccionado() {
+		return wifiSeleccionado;
+	}
+	
+	public void setWifiSeleccionado(boolean wifiSeleccionado) {
+		this.wifiSeleccionado = wifiSeleccionado;
+	}
+	
+	public boolean isAireSeleccionado() {
+		return aireSeleccionado;
+	}
+	
+	public void setAireSeleccionado(boolean aireSeleccionado) {
+		this.aireSeleccionado = aireSeleccionado;
+	}
+	
+	public boolean isPiscinaSeleccionada() {
+		return piscinaSeleccionada;
+	}
+	
+	public void setPiscinaSeleccionada(boolean piscinaSeleccionada) {
+		this.piscinaSeleccionada = piscinaSeleccionada;
+	}
+	
+	public boolean isSpaSeleccionado() {
+		return spaSeleccionado;
+	}
+	
+	public void setSpaSeleccionado(boolean spaSeleccionado) {
+		this.spaSeleccionado = spaSeleccionado;
+	}
+	
+	public boolean isGimnasioSeleccionado() {
+		return gimnasioSeleccionado;
+	}
+	
+	public void setGimnasioSeleccionado(boolean gimnasioSeleccionado) {
+		this.gimnasioSeleccionado = gimnasioSeleccionado;
+	}
+	
+	public boolean isParkingSeleccionado() {
+		return parkingSeleccionado;
+	}
+	
+	public void setParkingSeleccionado(boolean parkingSeleccionado) {
+		this.parkingSeleccionado = parkingSeleccionado;
+	}
+}

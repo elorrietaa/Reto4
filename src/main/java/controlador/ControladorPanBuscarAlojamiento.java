66c05b45bdff6f1cd3971b6cc064a54bba6a1a66
IN1DM3B_04 <IN1DM3B_04@H015112.NET>
2019-05-16 12:04:17 +0200

    Desabilitar botones en el jcalendar

--- src/main/java/controlador/ControladorPanBuscarAlojamiento.java
+++ src/main/java/controlador/ControladorPanBuscarAlojamiento.java
@@ -644,7 +644,7 @@ public class ControladorPanBuscarAlojamiento implements ActionListener, Property
 		//(0º)Se guardan los datos seleccionados en el modelo
 		if(vista.buscarAlojamiento.table.getSelectedRow() != -1) {
 			guardarDatosSeleccionadosAlojamiento();
-			if()
+			//if()
 				//(1º) se compueba la disponibilidad del alojamiento para las fechas seleccionadas.
 				validarCasaApartDisponible();
 			
